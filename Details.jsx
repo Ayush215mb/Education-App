e06688035134e4eca82abf59a3dc486495f24644
Ayush Yadav <sidharth197502@gmail.com>
2024-05-26 16:32:43 +0530

    Add files via upload

---
+++ Details.jsx
@@ -0,0 +1,10 @@
+// app/Screens/Details.tsx
+import { Text, View } from 'react-native';
+
+export default function Details() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Details Screen</Text>
+    </View>
+  );
+}
