ca48292509722e218645e2b4dac11b70178938ae
Ayush Yadav <sidharth197502@gmail.com>
2024-05-29 08:32:36 +0530

    Updated  LogIn.jsx

--- app/Screens/ LogIn.jsx
+++ app/Screens/ LogIn.jsx
@@ -1,147 +1,121 @@
-import { View, Text,Image,StyleSheet,Pressable,TextInput,Button,SafeAreaView} from "react-native";
-import { useState } from "react";
-import { Link } from "expo-router";
-import Colors from "../Colors/Colors";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import SignUp from "./SignUp";
-
-import { useNavigation } from "expo-router";
-import { TouchableOpacity } from "react-native-gesture-handler";
-const navigation = useNavigation();
+import { View, Text,Image,StyleSheet,TextInput,Pressable, } from 'react-native'
+import React from 'react'
+import { useState } from 'react';
+import { useRouter } from 'expo-router';
+import { auth } from '../../Firebase-Auth/firebaseconfig';
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 
-
-function LogIn({navigation}) {
-
-    const [username, setUsername] = useState("");//empty string
+const LogIn=()=> {
+    const [email, setEmail] = useState("");//empty string
     const [password, setPassword] = useState("");//empty string
-    const [errors, setErrors] = useState({})//empty object
-    
-    const ValidateForm = () => {
-        let errors = {}
-    
-        if (username == "") errors.username = "Username is required"
-    
-        if (password == "") errors.password = "password is required"
-    
-        setErrors(errors)
-    
-        return Object.keys(errors).length === 0;//this line of code checks if the object named errors is empty or  not, It returns true if the errors object is empty
+    const router = useRouter();
+
+    const LoginBtn= ()=>{
+        signInWithEmailAndPassword(auth, email, password)
+        .then(async (userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log("Loginnnn successful!")
+            router.push(`/Src/Screens/Home`)
+             // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+  });
     }
-    
-    const handlesubmit = () => {
-        //if there are no errors then the validateform funcn returns true thus the next step will be taken
+  return (
+    <View style={{ backgroundColor: "white",flex:1 }}>
+
+        <Image source={require('../Images/login.png')} />
+
+        <View style={Styles.LogInContainer}>
+          <Text style={Styles.WelcomeText} > Welcome to Education App </Text>
+           <View style={{ paddingHorizontal: 10, paddingTop: 10 }}>
+              <TextInput
+                placeholder="Enter your Email"
+                style={Styles.input}
+                value={email}
+                onChangeText={setEmail}
+                autoCapitalize="none"
+                />
+           </View>
+           <View style={{ paddingHorizontal: 10, paddingTop: 10 }}>
+             <TextInput
+                placeholder="Enter your Password"
+                style={Styles.input}
+                value={password}
+                onChangeText={setPassword}
+                // secureTextEntry
+             />
+            </View>
+            <Pressable onPress={LoginBtn}>
+                <View style={Styles.ButtonView}>              
+                    <Text style={Styles.ButtonText} >Log in</Text>               
+                </View>
+            </Pressable>
             
-        if (ValidateForm()) {
-            console.log("submitted", username, password)
-            setUsername("");
-            setPassword("");
-            setErrors({});
-        }
-            // console.log("submitted", username, password)
-            // setUsername("");
-            // setPassword("");
-    
-    }
-    return(
-            <SafeAreaView style={{ backgroundColor: "white",flex:1 }}>
-    
-                <Image source={require("../Images/login.png")} style={styles.Image} />
-
-                <View style={styles.container} >
-                    <Text style={styles.WelcomeText} > Welcome to Education App </Text>
-    
-    
-                    <View style={{ paddingHorizontal: 10, paddingTop: 10 }}>
-                        <TextInput
-                            placeholder="Enter your Username"
-                            style={styles.input}
-                            value={username}
-                            onChangeText={setUsername}
-                            autoCapitalize="none"
-                        />
-    
-                        {errors.username ? (
-                            <Text >{errors.username}</Text>
-                        ) : null
-                        }
-                    </View>
-    
-                    <View style={{ paddingHorizontal: 10, paddingTop: 10 }}>
-
-                        <TextInput
-                            placeholder="Enter your Password"
-                            style={styles.input}
-                            value={password}
-                            onChangeText={setPassword}
-                            secureTextEntry
-                        />
-                        
-                        {errors.password ? (
-                            <Text >{errors.password}</Text>
-                        ) : null
-                        }
-                    </View>
+            <Pressable onPress={()=> router.push(`/Src/Screens/SignUp`) } >
+                {/* we have to give the file location relative to the app directory */}
+                <View style={Styles.SignUpButton}>
+                    <Text style={Styles.ButtonText}>Sign up</Text>
                 </View>
-    
-                <View style={styles.ButtonView}>
-                    <Pressable onPress={handlesubmit}>
-                        <Text style={styles.ButtonText} >Log in</Text>
-                    </Pressable>
-                </View>
-    
-                {/* <Link href={{pathname:"/../Src/Screens/SignUp"}} asChild>
-                    <Pressable onPress={()=>navigation.navigate('root', {screen:"SignUp"} )} >
-                    <Pressable>
-                        <View style={styles.SignUpButton}>
-                            <Text style={styles.ButtonText}>Sign Up</Text>
-                        </View>    
-                    </Pressable>
-                </Link> */}
-                <>
-                <TouchableOpacity onPress={()=> navigation.navigate("SignUp")} >
-                        <View style={styles.SignUpButton}>
-                            <Text style={styles.ButtonText}>Sign Up</Text>
-                        </View> 
+            </Pressable>
 
-                </TouchableOpacity>
-                </>
-                
-            </SafeAreaView>
-    
-        )
-    
-    }
+            
+       </View>
+    </View>
+  )
+}
 
 export default LogIn;
 
-const styles = StyleSheet.create(
+const Styles= StyleSheet.create(
     {
-        container: {
-            
+        Image:{
+            alignItems: "center",
+            alignContent: "center"
+        },
+        LogInContainer:{
             backgroundColor: "white",
-            paddingTop: 40,
+            paddingTop: 30,
             marginTop: -20,
             borderTopLeftRadius: 30,
             borderTopRightRadius: 30,
-
-        },
-        Image: {
-            alignItems: "center",
-            alignContent: "center"
         },
         WelcomeText: {
             fontSize: 30,
             fontWeight: "bold",
             textAlign: "center",
         },
+        input: {
+            marginTop: 40,
+            borderWidth: 1,
+            padding: 5,
+            paddingHorizontal: 10,
+            borderColor: "black",
+            fontSize: 15,
+            borderRadius: 15,
+            
+        },
+        input: {
+            marginTop: 40,
+            borderWidth: 1,
+            padding: 5,
+            paddingHorizontal: 10,
+            borderColor: "black",
+            fontSize: 15,
+            borderRadius: 15,
+            
+        },
         ButtonView: {
             alignSelf: "center",
-            backgroundColor: Colors.Primary,
+            backgroundColor: "#0C7DE4",
             marginTop: 40,
             padding: 10,
-            paddingHorizontal:30,
-            borderRadius: 20,
+            paddingHorizontal:50,
+            borderRadius: 30,
             display: "flex",
             flexDirection: "row",
             justifyContent: "center",
@@ -155,30 +129,17 @@ const styles = StyleSheet.create(
             // backgroundColor:Colors.Primary,
 
         },
-        icon: {
-            marginRight: 20
-        },
-        input: {
-            marginTop: 40,
-            borderWidth: 1,
-            padding: 5,
-            paddingHorizontal: 10,
-            borderColor: "black",
-            fontSize: 15,
-            borderRadius: 15,
-            
-        },
         SignUpButton:{
             display: "flex",
             flexDirection: "row",
             justifyContent: "center",
             alignItems: "center",
             alignSelf: "center",
-            backgroundColor: Colors.Primary,
-            marginTop: 10,
+            backgroundColor:"#0C7DE4",
+            marginTop: 15,
             padding: 10,
-            paddingHorizontal:30,
-            borderRadius: 20,
+            paddingHorizontal:50,
+            borderRadius:30,
       
         }
     }
